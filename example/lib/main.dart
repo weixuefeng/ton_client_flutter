40e5d47956895737560083271d00f97c88cfa425
pony <weixuefeng1018@gmail.com>
2024-04-11 16:59:27 +0800

    feat: add address generate test

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -21,7 +21,23 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   String _platformVersion = 'Unknown';
   final _tonClientFlutterPlugin = TonClientFlutter();
-
+  var CLIENT_DEFAULT_SETUP = {
+        'network': {
+          'endpoints': ['http://localhost'],
+          'message_retries_count': 5,
+          'message_processing_timeout': 40000,
+          'wait_for_timeout': 40000,
+          'out_of_sync_threshold': 15000,
+          'access_key': ''
+        },
+        'crypto': {'fish_param': ''},
+        'abi': {
+          'message_expiration_timeout': 40000,
+          'message_expiration_timeout_grow_factor': 1.5
+        }
+      };
+      var client = TonClient();
+  var everCode = "te6cckEBBgEA/AABFP8A9KQT9LzyyAsBAgEgAgMABNIwAubycdcBAcAA8nqDCNcY7UTQgwfXAdcLP8j4KM8WI88WyfkAA3HXAQHDAJqDB9cBURO68uBk3oBA1wGAINcBgCDXAVQWdfkQ8qj4I7vyeWa++COBBwiggQPoqFIgvLHydAIgghBM7mRsuuMPAcjL/8s/ye1UBAUAmDAC10zQ+kCDBtcBcdcBeNcB10z4AHCAEASqAhSxyMsFUAXPFlAD+gLLaSLQIc8xIddJoIQJuZgzcAHLAFjPFpcwcQHLABLM4skB+wAAPoIQFp4+EbqOEfgAApMg10qXeNcB1AL7AOjRkzLyPOI+zYS/";
   @override
   void initState() {
     super.initState();
@@ -39,7 +55,6 @@ class _MyAppState extends State<MyApp> {
     } on PlatformException {
       platformVersion = 'Failed to get platform version.';
     }
-    print("object");
 
     // If the widget was removed from the tree while the asynchronous platform
     // message was in flight, we want to discard the reply rather than calling
@@ -51,35 +66,29 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
-  Future<void> onClick() async {
-    print("-----");
-      var client = TonClient();
-      var config = ClientConfig(abi: AbiConfig(workchain: 0, message_expiration_timeout: 40000, message_expiration_timeout_grow_factor: 2), 
-      binding: BindingConfig(library: "libton_client", version: "1.45"), 
-      boc: BocConfig(cache_max_size: 10240),
-      proofs: ProofsConfig(cache_in_local_storage: false),
-      network: NetworkConfig(endpoints: [""], message_retries_count: 5, message_processing_timeout: 40000, wait_for_timeout: 40000, out_of_sync_threshold: 15000));
-var CLIENT_DEFAULT_SETUP = {
-      'network': {
-        'endpoints': ['http://localhost'],
-        'message_retries_count': 5,
-        'message_processing_timeout': 40000,
-        'wait_for_timeout': 40000,
-        'out_of_sync_threshold': 15000,
-        'access_key': ''
-      },
-      'crypto': {'fish_param': ''},
-      'abi': {
-        'message_expiration_timeout': 40000,
-        'message_expiration_timeout_grow_factor': 1.5
-      }
-    };
+  Future<void> generateAddress() async {
 
+  }
+
+  Future<void> onClick() async {
     client.connect(CLIENT_DEFAULT_SETUP);
-      var boc =
-          'te6ccgEBAQEAWAAAq2n+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAE/zMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzSsG8DgAAAAAjuOu9NAL7BxYpA';
-      var res = await client.boc.parse_message(ParamsOfParse(boc: boc));
-      print(res);
+    var keypair = await client.crypto.generate_random_sign_keys();
+    var encodeBoc = await client.abi.encode_boc(ParamsOfAbiEncodeBoc(params: [
+      AbiParam(name: "publicKey", type: "uint256"),
+      AbiParam(name: "timestamp", type: "uint64")
+    ], data: {
+      "publicKey": "0x${keypair.public!}",
+      "timestamp": 0
+    }));
+    var initData = encodeBoc.boc!;
+    var encodeStateInit = await client.boc.encode_state_init(ParamsOfEncodeStateInit(
+      code: everCode,
+      data: initData
+    ));
+    var stateInit = encodeStateInit.state_init!;
+    var hash = await client.boc.get_boc_hash(ParamsOfGetBocHash(boc: stateInit));
+    var address = "0:$hash";
+    print(address);
     client.disconnect();
   }
 
